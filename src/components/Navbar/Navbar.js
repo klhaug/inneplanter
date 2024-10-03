b2f0dc6039deea91a9958eb74e1d2e204669e22d
klhaug <kristian@snougle.no>
2024-10-03 18:55:40 +0200

    created navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,20 @@
 import React from "react";
+import "./navbar.css";
 
 function Navbar() {
     return(
-        <div>
-            <nav>
-                <ul>
-                    <li>Hjem</li>
-                </ul>
-            </nav>
-        </div>
+        <div className="navbar-container">
+
+                <nav className="navbar">
+                    <div className="logo-container">
+                        <a href='#hjem'>Inneplanter</a>
+                    </div>
+                    <ul className="nav-right-container">
+                        <li>Hjem</li>
+                        <li>Planter</li>
+                    </ul>
+                </nav>
+            </div>
     );
 }
 
