26bf866ad2932989677c54e8951a8c436a23f9c8
klhaug <kristian@snougle.no>
2024-10-03 18:13:35 +0200

    pushing

---
+++ src/containers/App/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from '../../components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
